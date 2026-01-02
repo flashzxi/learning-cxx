efc253e9d4d83ca9b29756a4e0235535df09c3ac
flashzxi <39670222+flashzxi@users.noreply.github.com>
2026-01-03 01:03:21 +0800

    merge cxx-learning pr
    * finish exercises
    * fit wsl

--- exercises/27_strides/main.cpp
+++ exercises/27_strides/main.cpp
@@ -18,6 +18,15 @@ std::vector<udim> strides(std::vector<udim> const &shape) {
     // TODO: 完成函数体，根据张量形状计算张量连续存储时的步长。
     // READ: 逆向迭代器 std::vector::rbegin <https://zh.cppreference.com/w/cpp/container/vector/rbegin>
     //       使用逆向迭代器可能可以简化代码
+    int step = 1;
+    auto rit = strides.rbegin();
+    auto shape_rit = shape.rbegin();
+    while (rit != strides.rend()) {
+        *rit = step;
+        step *= *shape_rit;
+        ++shape_rit;
+        ++rit;
+    }
     return strides;
 }
 
